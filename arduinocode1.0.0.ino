c491b89b4af99b7c3b343766c630ffa2564c3397
Matteo Rettore <60564373+ilkiwer@users.noreply.github.com>
2020-05-07 09:27:40 +0200

    Update arduinocode1.0.0.ino
    Corretta la ricezione da Py, formattato correttamente.

--- arduinocode1.0.0.ino
+++ arduinocode1.0.0.ino
@@ -7,13 +7,13 @@ String string = "";
 char mosse[200];
 char mossa;
 int inv = 0, passiU, passiL, passiB, passiR, passiF, passiD,
-    esci = 0, totale = 0, rotX = 0, rotY = 0, rotZ=0;
-int pinU[] = {1, 2};
-int pinL[] = {3, 4};
-int pinB[] = {5, 6};
-int pinR[] = {7, 8};
-int pinD[] = {9, 10};
-int pinF[] = {11, 12};
+  esci = 0, totale = 0, rotX = 0, rotY = 0, rotZ = 0;
+int pinU[] = {1,2};
+int pinL[] = {3,4};
+int pinB[] = {5,6};
+int pinR[] = {7,8};
+int pinD[] = {9,10};
+int pinF[] = {11,12};
 
 Stepper serU(PASSI, pinU[0], pinU[1]);
 Stepper serL(PASSI, pinL[0], pinL[1]);
@@ -21,7 +21,6 @@ Stepper serB(PASSI, pinB[0], pinB[1]);
 Stepper serR(PASSI, pinR[0], pinR[1]);
 Stepper serD(PASSI, pinD[0], pinD[1]);
 Stepper serF(PASSI, pinF[0], pinF[1]);
-
 
 void setup() {
   serU.setSpeed(VELOCITA);
@@ -39,20 +38,19 @@ void loop() {
     //carattere in input
     char character;
     //Prendi i dati, passa in carattere e poi aggiungi a string. Conta.
-     while (!Serial.available()) {}
-      while (Serial.available())
-     {
+    while (!Serial.available()) {}
+    while (Serial.available()) {
       character = Serial.read();
       string.concat(character);
       totale++;
       delay(10);
-     }
-     //Porta tutto ad array
-     string.toCharArray(mosse, 200);
-     delay(200);
-     //OPZ 
-     Serial.println(mosse);
-  //Leggi un comando completo alla volta
+    }
+    //Porta tutto ad array
+    string.toCharArray(mosse, 200);
+    delay(200);
+    //OPZ 
+    Serial.println(mosse);
+    //Leggi un comando completo alla volta
     for (int i = 0; i <= totale; i++) {
       if (mosse[i] == ' ')
         i++;
@@ -75,160 +73,160 @@ void loop() {
       */
 
       switch (mossa) {
-        case 'U':
-          if (rotX == 0 && rotZ == 0)
-            passiU = PASSI / 4;
-          else if (rotX == 1 && rotZ == 0)
-            passiF = PASSI / 4;
-          else if (rotX == 2 && rotZ == 0)
-            passiD = PASSI / 4;
-          else if (rotX == 3 && rotZ == 0)
-            passiB = PASSI / 4;
-          if (rotZ == 0 && rotX == 0)
-            passiU = PASSI / 4;
-          else if (rotZ == 1 && rotX == 0)
-            passiL = PASSI / 4;
-          else if (rotZ == 2 && rotX == 0)
-            passiD = PASSI / 4;
-          else if (rotZ == 3 && rotX == 0)
-            passiR = PASSI / 4;
-          break;
+      case 'U':
+        if (rotX == 0 && rotZ == 0)
+          passiU = PASSI / 4;
+        else if (rotX == 1 && rotZ == 0)
+          passiF = PASSI / 4;
+        else if (rotX == 2 && rotZ == 0)
+          passiD = PASSI / 4;
+        else if (rotX == 3 && rotZ == 0)
+          passiB = PASSI / 4;
+        if (rotZ == 0 && rotX == 0)
+          passiU = PASSI / 4;
+        else if (rotZ == 1 && rotX == 0)
+          passiL = PASSI / 4;
+        else if (rotZ == 2 && rotX == 0)
+          passiD = PASSI / 4;
+        else if (rotZ == 3 && rotX == 0)
+          passiR = PASSI / 4;
+        break;
 
-        case 'L':
-          if (rotY == 0 && rotZ == 0)
-            passiL = PASSI / 4;
-          else if (rotY == 1 && rotZ == 0)
-            passiF = PASSI / 4;
-          else if (rotY == 2 && rotZ == 0)
-            passiR = PASSI / 4;
-          else if (rotY == 3  && rotZ == 0)
-            passiB = PASSI / 4;
-          if (rotZ == 0 && rotY == 0)
-            passiL = PASSI / 4;
-          else if (rotZ == 1 && rotY == 0)
-            passiD = PASSI / 4;
-          else if (rotZ == 2 && rotY == 0)
-            passiR = PASSI / 4;
-          else if (rotZ == 3  && rotY == 0)
-            passiU = PASSI / 4;
-          break;
+      case 'L':
+        if (rotY == 0 && rotZ == 0)
+          passiL = PASSI / 4;
+        else if (rotY == 1 && rotZ == 0)
+          passiF = PASSI / 4;
+        else if (rotY == 2 && rotZ == 0)
+          passiR = PASSI / 4;
+        else if (rotY == 3 && rotZ == 0)
+          passiB = PASSI / 4;
+        if (rotZ == 0 && rotY == 0)
+          passiL = PASSI / 4;
+        else if (rotZ == 1 && rotY == 0)
+          passiD = PASSI / 4;
+        else if (rotZ == 2 && rotY == 0)
+          passiR = PASSI / 4;
+        else if (rotZ == 3 && rotY == 0)
+          passiU = PASSI / 4;
+        break;
 
-        case 'B':
-          if (rotX == 0  && rotY == 0)
-            passiB = PASSI / 4;
-          else if (rotX == 1 && rotY == 0)
-            passiU = PASSI / 4;
-          else if (rotX == 2 && rotY == 0)
-            passiF = PASSI / 4;
-          else if (rotX == 3 && rotY == 0)
-            passiD = PASSI / 4;
-          if (rotY == 0 && rotX == 0)
-            passiB = PASSI / 4;
-          else if (rotY == 1 && rotX == 0)
-            passiL = PASSI / 4;
-          else if (rotY == 2 && rotX == 0)
-            passiF = PASSI / 4;
-          else if (rotY == 3  && rotX == 0)
-            passiR = PASSI / 4;
-          break;
+      case 'B':
+        if (rotX == 0 && rotY == 0)
+          passiB = PASSI / 4;
+        else if (rotX == 1 && rotY == 0)
+          passiU = PASSI / 4;
+        else if (rotX == 2 && rotY == 0)
+          passiF = PASSI / 4;
+        else if (rotX == 3 && rotY == 0)
+          passiD = PASSI / 4;
+        if (rotY == 0 && rotX == 0)
+          passiB = PASSI / 4;
+        else if (rotY == 1 && rotX == 0)
+          passiL = PASSI / 4;
+        else if (rotY == 2 && rotX == 0)
+          passiF = PASSI / 4;
+        else if (rotY == 3 && rotX == 0)
+          passiR = PASSI / 4;
+        break;
 
-        case 'R':
-          if (rotY == 0 && rotZ == 0)
-            passiR = PASSI / 4;
-          else if (rotY == 1 && rotZ == 0)
-            passiB = PASSI / 4;
-          else if (rotY == 2 && rotZ == 0)
-            passiL = PASSI / 4;
-          else if (rotY == 3 && rotZ == 0)
-            passiF = PASSI / 4;
-          if (rotZ == 0 && rotY == 0)
-            passiR = PASSI / 4;
-          else if (rotZ == 1 && rotY == 0)
-            passiU = PASSI / 4;
-          else if (rotZ == 2 && rotY == 0)
-            passiL = PASSI / 4;
-          else if (rotZ == 3 && rotY == 0)
-            passiD = PASSI / 4;
-          break;
+      case 'R':
+        if (rotY == 0 && rotZ == 0)
+          passiR = PASSI / 4;
+        else if (rotY == 1 && rotZ == 0)
+          passiB = PASSI / 4;
+        else if (rotY == 2 && rotZ == 0)
+          passiL = PASSI / 4;
+        else if (rotY == 3 && rotZ == 0)
+          passiF = PASSI / 4;
+        if (rotZ == 0 && rotY == 0)
+          passiR = PASSI / 4;
+        else if (rotZ == 1 && rotY == 0)
+          passiU = PASSI / 4;
+        else if (rotZ == 2 && rotY == 0)
+          passiL = PASSI / 4;
+        else if (rotZ == 3 && rotY == 0)
+          passiD = PASSI / 4;
+        break;
 
-        case 'F':
-          if (rotX == 0  && rotY == 0)
-            passiF = PASSI / 4;
-          else if (rotX == 1  && rotY == 0)
-            passiD = PASSI / 4;
-          else if (rotX == 2  && rotY == 0)
-            passiB = PASSI / 4;
-          else if (rotX == 3   && rotY == 0)
-            passiU = PASSI / 4;
-          if (rotY == 0  && rotX == 0)
-            passiF = PASSI / 4;
-          else if (rotY == 1  && rotX == 0)
-            passiR = PASSI / 4;
-          else if (rotY == 2  && rotX == 0)
-            passiB = PASSI / 4;
-          else if (rotY == 3  && rotX == 0)
-            passiL = PASSI / 4;
-          break;
+      case 'F':
+        if (rotX == 0 && rotY == 0)
+          passiF = PASSI / 4;
+        else if (rotX == 1 && rotY == 0)
+          passiD = PASSI / 4;
+        else if (rotX == 2 && rotY == 0)
+          passiB = PASSI / 4;
+        else if (rotX == 3 && rotY == 0)
+          passiU = PASSI / 4;
+        if (rotY == 0 && rotX == 0)
+          passiF = PASSI / 4;
+        else if (rotY == 1 && rotX == 0)
+          passiR = PASSI / 4;
+        else if (rotY == 2 && rotX == 0)
+          passiB = PASSI / 4;
+        else if (rotY == 3 && rotX == 0)
+          passiL = PASSI / 4;
+        break;
 
-        case 'D':
-          if (rotX == 0  && rotZ == 0)
-            passiD = PASSI / 4;
-          else if (rotX == 1  && rotZ == 0)
-            passiB = PASSI / 4;
-          else if (rotX == 2  && rotZ == 0)
-            passiU = PASSI / 4;
-          else if (rotX == 3  && rotZ == 0)
-            passiF = PASSI / 4;
-          if (rotZ == 0  && rotX == 0)
-            passiD = PASSI / 4;
-          else if (rotZ == 1  && rotX == 0)
-            passiR = PASSI / 4;
-          else if (rotZ == 2  && rotX == 0)
-            passiU = PASSI / 4;
-          else if (rotZ == 3  && rotX == 0)
-            passiL = PASSI / 4;
-          break;
+      case 'D':
+        if (rotX == 0 && rotZ == 0)
+          passiD = PASSI / 4;
+        else if (rotX == 1 && rotZ == 0)
+          passiB = PASSI / 4;
+        else if (rotX == 2 && rotZ == 0)
+          passiU = PASSI / 4;
+        else if (rotX == 3 && rotZ == 0)
+          passiF = PASSI / 4;
+        if (rotZ == 0 && rotX == 0)
+          passiD = PASSI / 4;
+        else if (rotZ == 1 && rotX == 0)
+          passiR = PASSI / 4;
+        else if (rotZ == 2 && rotX == 0)
+          passiU = PASSI / 4;
+        else if (rotZ == 3 && rotX == 0)
+          passiL = PASSI / 4;
+        break;
 
         /////////////////////////ROTAZIONI
-        case 'X':
-          rotX++;
-          if (rotX == 4)
-            rotX = 0;
-          break;
+      case 'X':
+        rotX++;
+        if (rotX == 4)
+          rotX = 0;
+        break;
 
-        case 'Y':
-          rotY++;
-          if (rotY == 4)
-            rotY = 0;
-          break;
+      case 'Y':
+        rotY++;
+        if (rotY == 4)
+          rotY = 0;
+        break;
 
-        case 'Z':
-          rotZ++;
-          if (rotZ == 4)
-            rotZ = 0;
-          break;
+      case 'Z':
+        rotZ++;
+        if (rotZ == 4)
+          rotZ = 0;
+        break;
 
-        case 'H':
-          rotX--;
-          if (rotX < 0)
-            rotX = 3;
-          break;
+      case 'H':
+        rotX--;
+        if (rotX < 0)
+          rotX = 3;
+        break;
 
-        case 'I':
-          rotY--;
-          if (rotY < 0)
-            rotY = 3;
-          break;
+      case 'I':
+        rotY--;
+        if (rotY < 0)
+          rotY = 3;
+        break;
 
-        case 'J':
-          rotZ--;
-          if (rotZ < 0)
-            rotZ = 3;
-          break;
+      case 'J':
+        rotZ--;
+        if (rotZ < 0)
+          rotZ = 3;
+        break;
 
-        case 'O': //O di ornitorinco
-          esci = 1;
-          break;
+      case 'O': //O di ornitorinco
+        esci = 1;
+        break;
       }
       if (inv == 1) {
         passiU = 0 - passiU;
